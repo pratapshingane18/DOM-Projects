2bf94f2eba7a7941afa1275636b35481fdd11699
pratapshingane18 <sushrutrao465@gmail.com>
2023-01-16 11:52:00 +0530

    Task add complete

--- app.js
+++ app.js
@@ -30,6 +30,30 @@ function addTask(e){
     const li = document.createElement('li');
 
     // Add a class
+    li.className = 'collection-item';
+
+    // Create a text node and append to li
+
+    li.appendChild(document.createTextNode(taskInput.value));
+
+    // Create nre Link Element:
+    const link = document.createElement('a');
+
+    // Add a class
+    link.className = 'delete-item secondary-content';
+
+    // Add Icon HTML
+
+    link.innerHTMl = '<i class="fa-fa-remove"> </i>'
+
+    // Append the link to li
+    li.appendChild(link);
+
+    // Append li to ul
+    taskList.appendChild(li);
+
+    // Clear Input
+     taskInput.value = '';
     
     e.preventDefault();
 }
